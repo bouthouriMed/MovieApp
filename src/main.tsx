025d0f8b00e9f9fe09ca1475f1690d9944a9b3fc
Mohamed <bouthourimed76@gmail.com>
2025-08-28 14:55:58 +0200

    fix hash

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter basename="/MovieApp">
+    <HashRouter>
       <Provider store={store}>
         <App />
         <ToastContainer
